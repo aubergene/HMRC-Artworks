d1f804a024be0ca54e8f08d008f745dc76bd33a2
Ben Griffiths <bengriffiths@gmail.com>
2010-03-27 17:07:29 +0000

    Partial for artworks

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,5 +3,29 @@ class Artwork < ActiveRecord::Base
   belongs_to  :contact
   has_many    :visit_requests
   
+  def categorize
+    case self.category
+    when 'Books & Manuscripts'
+      '[BOOK]'
+    when 'European Ceramics & Glass'
+      '[CERAMIC]'
+    when 'Metalwork and Silver'
+      '[METAL]'
+    when 'Miscellaneous'
+      ''
+    when 'Paintings & Miniatures'
+      '[PAINT]'
+    when 'Prints and Drawings'
+      '[PRINT]'
+    when 'Watches & Sci.Instruments'
+      '[SCIENCE]'
+    else 
+      '['+self.category.upcase+']'
+    end
+  end
+
+  def friendly_html_description
+    '<span class="category">'+self.categorize+"</span> "+self.description
+  end
 
 end
